3ac72efc1f2f937e60fa2ae25dea82964c88d9f8
Denys <nikeenko89@gmail.com>
2026-09-12 18:44:16 +0200

    LessonSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,23 +3,30 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели курса."""
-
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ["id", "name", "description", "lessons_count"]
-
-    def get_lessons_count(self, obj):
-        """Возвращает количество уроков в курсе."""
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для модели урока."""
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели курса."""
+
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "name",
+            "description",
+            "lessons_count",
+            "lessons",
+        ]
+
+    def get_lessons_count(self, obj):
+        """Возвращает количество уроков в курсе."""
+        return obj.lessons.count()
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from courses.models import Course, Lesson
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
